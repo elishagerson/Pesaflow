5e0838c82661fd28f77d8484661cf028a3f4f82c
Elisha Gerson <ndunduluelisha@gmail.com>
2026-05-21 10:39:37 +0200

    fix: Update build.gradle.kts for Java compiler issue

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -22,3 +22,23 @@ subprojects {
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
+
+subprojects {
+    val configureNamespace = Action<Project> {
+        if (plugins.hasPlugin("com.android.library")) {
+            val android = extensions.findByName("android") as? com.android.build.gradle.BaseExtension
+            if (android != null && android.namespace == null) {
+                android.namespace = "com.pesaflow." + name.replace("-", "_")
+                if (name == "telephony") {
+                    android.namespace = "com.shounakmulay.telephony"
+                }
+            }
+        }
+    }
+
+    if (state.executed) {
+        configureNamespace.execute(this)
+    } else {
+        afterEvaluate(configureNamespace)
+    }
+}
