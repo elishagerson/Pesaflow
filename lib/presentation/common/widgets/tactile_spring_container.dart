603c3208410a13fa07c79597197aea8fd5a2b4f5
Elisha Gerson <ndunduluelisha@gmail.com>
2026-06-01 14:11:50 +0300

    Update tactile_spring_container.dart

--- lib/presentation/common/widgets/tactile_spring_container.dart
+++ lib/presentation/common/widgets/tactile_spring_container.dart
@@ -1,19 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/physics.dart';
 
 class TactileSpringContainer extends StatefulWidget {
   final Widget child;
   final VoidCallback? onTap;
   final double scaleFactor;
-  final Duration duration;
-  final Curve curve;
 
   const TactileSpringContainer({
     super.key,
     required this.child,
     this.onTap,
     this.scaleFactor = 0.96,
-    this.duration = const Duration(milliseconds: 100),
-    this.curve = Curves.easeInOut,
   });
 
   @override
@@ -28,11 +25,11 @@ class _TactileSpringContainerState extends State<TactileSpringContainer>
   @override
   void initState() {
     super.initState();
-    _controller = AnimationController(vsync: this, duration: widget.duration);
+    _controller = AnimationController(vsync: this);
     _scaleAnimation = Tween<double>(
       begin: 1.0,
       end: widget.scaleFactor,
-    ).animate(CurvedAnimation(parent: _controller, curve: widget.curve));
+    ).animate(_controller);
   }
 
   @override
@@ -41,17 +38,36 @@ class _TactileSpringContainerState extends State<TactileSpringContainer>
     super.dispose();
   }
 
+  void _pressDown() {
+    if (widget.onTap == null) return;
+    _controller.animateTo(
+      1.0,
+      duration: const Duration(milliseconds: 100),
+      curve: Curves.easeOutCubic,
+    );
+  }
+
+  void _springBack() {
+    if (widget.onTap == null) return;
+    const spring = SpringDescription(
+      mass: 0.8,
+      stiffness: 350.0,
+      damping: 14.0,
+    );
+    final simulation = SpringSimulation(spring, _controller.value, 0.0, 0.0);
+    _controller.animateWith(simulation);
+  }
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTapDown: widget.onTap != null ? (_) => _controller.forward() : null,
-      onTapUp: widget.onTap != null
-          ? (_) {
-              _controller.reverse();
-              widget.onTap!();
-            }
-          : null,
-      onTapCancel: widget.onTap != null ? () => _controller.reverse() : null,
+      behavior: HitTestBehavior.opaque,
+      onTapDown: (_) => _pressDown(),
+      onTapUp: (_) {
+        _springBack();
+        widget.onTap?.call();
+      },
+      onTapCancel: () => _springBack(),
       child: ScaleTransition(scale: _scaleAnimation, child: widget.child),
     );
   }
