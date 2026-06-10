3d14244a09ec840d03afa51647d80268fbad1603
Elisha Gerson <ndunduluelisha@gmail.com>
2026-06-10 09:12:36 +0300

    fix: update PesaFlow files

--- lib/presentation/sms_review/sms_review_screen.dart
+++ lib/presentation/sms_review/sms_review_screen.dart
@@ -15,56 +15,6 @@ import 'package:pesaflow/presentation/state/state_providers.dart';
 class SmsReviewScreen extends ConsumerWidget {
   const SmsReviewScreen({super.key});
 
-  IconData getCategoryIcon(String iconName) {
-    switch (iconName) {
-      case 'briefcase':
-        return Icons.work_rounded;
-      case 'store':
-        return Icons.storefront_rounded;
-      case 'cart':
-        return Icons.shopping_cart_rounded;
-      case 'bus':
-        return Icons.directions_bus_rounded;
-      case 'home':
-        return Icons.home_rounded;
-      case 'zap':
-        return Icons.electric_bolt_rounded;
-      case 'phone':
-        return Icons.phone_android_rounded;
-      case 'heart':
-        return Icons.favorite_rounded;
-      case 'book':
-        return Icons.menu_book_rounded;
-      case 'film':
-        return Icons.movie_rounded;
-      case 'shopping-bag':
-        return Icons.shopping_bag_rounded;
-      case 'coffee':
-        return Icons.coffee_rounded;
-      case 'send':
-        return Icons.send_rounded;
-      case 'credit-card':
-        return Icons.credit_card_rounded;
-      case 'banknote':
-        return Icons.payments_rounded;
-      case 'piggy-bank':
-        return Icons.savings_rounded;
-      case 'arrow-left-right':
-        return Icons.compare_arrows_rounded;
-      case 'plus-circle':
-      default:
-        return Icons.add_circle_outline_rounded;
-    }
-  }
-
-  Color hexToColor(String hex) {
-    final clean = hex.replaceAll('#', '');
-    if (clean.length == 6) {
-      return Color(int.parse('FF$clean', radix: 16));
-    }
-    return Colors.grey;
-  }
-
   void _showCategoryPicker(
     BuildContext context,
     WidgetRef ref,
