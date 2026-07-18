d7e394c6351390da9c8ea92d0ce2263525ebd371
Elisha Gerson <ndunduluelisha@gmail.com>
2026-07-18 18:47:26 +0300

    fix: update transaction list screen

--- lib/presentation/transactions/transaction_list_screen.dart
+++ lib/presentation/transactions/transaction_list_screen.dart
@@ -22,7 +22,6 @@ import 'package:pesaflow/core/utils/app_illustrations.dart';
 import 'package:pesaflow/presentation/common/widgets/empty_state.dart';
 import 'package:pesaflow/presentation/common/widgets/staggered_animation.dart';
 import 'package:pesaflow/presentation/common/widgets/staggered_list.dart';
-import 'package:pesaflow/presentation/common/widgets/custom_toast.dart';
 import 'package:pesaflow/presentation/common/widgets/undo_delete.dart';
 import 'package:pesaflow/presentation/state/state_providers.dart';
 
