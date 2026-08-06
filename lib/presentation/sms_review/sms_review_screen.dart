53740e12fa8f4ec85162294368376cb47d2dafe8
Elisha Gerson <ndunduluelisha@gmail.com>
2026-08-06 17:04:45 +0300

    fix: update sms review screen

--- lib/presentation/sms_review/sms_review_screen.dart
+++ lib/presentation/sms_review/sms_review_screen.dart
@@ -20,6 +20,7 @@ import 'package:pesaflow/presentation/common/widgets/empty_state.dart';
 import 'package:pesaflow/presentation/common/widgets/custom_toast.dart';
 import 'package:pesaflow/presentation/common/widgets/add_category_dialog.dart';
 import 'package:pesaflow/presentation/common/widgets/undo_delete.dart';
+import 'package:pesaflow/presentation/common/widgets/motion/success_checkmark.dart';
 
 class SmsReviewScreen extends ConsumerStatefulWidget {
   const SmsReviewScreen({super.key});
@@ -438,11 +439,10 @@ class _SmsReviewScreenState extends ConsumerState<SmsReviewScreen> {
                                   recentTransactionsStreamProvider,
                                 );
                                 if (context.mounted) {
-                                  CustomToast.show(
+                                  SuccessCheckmark.show(
                                     context,
                                     message:
                                         'Transaction approved: ${trans.description}',
-                                    type: ToastType.success,
                                   );
                                 }
                               },
@@ -740,11 +740,10 @@ class _SmsReviewScreenState extends ConsumerState<SmsReviewScreen> {
                                                   recentTransactionsStreamProvider,
                                                 );
                                                 if (context.mounted) {
-                                                  CustomToast.show(
+                                                  SuccessCheckmark.show(
                                                     context,
                                                     message:
                                                         'Transaction approved',
-                                                    type: ToastType.success,
                                                   );
                                                 }
                                               },
@@ -986,11 +985,10 @@ class _SmsReviewScreenState extends ConsumerState<SmsReviewScreen> {
                                         _selectAll = false;
                                       });
                                       if (context.mounted) {
-                                        CustomToast.show(
+                                        SuccessCheckmark.show(
                                           context,
                                           message:
                                               '$approvedCount transactions approved',
-                                          type: ToastType.success,
                                         );
                                       }
                                     },
