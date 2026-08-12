f62db7104eb875dcbdcca7d206d9d3fc8870ef73
Elisha Gerson <ndunduluelisha@gmail.com>
2026-08-12 08:33:16 +0300

    Update app router

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:pesaflow/core/theme/app_colors_theme.dart';
 import 'package:pesaflow/presentation/analytics/analytics_screen.dart';
