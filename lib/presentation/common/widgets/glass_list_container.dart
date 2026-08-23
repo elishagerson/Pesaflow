05c4cd4c90113088769fd0db1872c401f6c1500e
Elisha Gerson <ndunduluelisha@gmail.com>
2026-08-23 21:11:42 +0300

    fix: update glass list container

--- lib/presentation/common/widgets/glass_list_container.dart
+++ lib/presentation/common/widgets/glass_list_container.dart
@@ -1,30 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:pesaflow/core/utils/context_extensions.dart';
 import 'package:pesaflow/presentation/common/widgets/liquid_glass.dart';
 
 class GlassListContainer extends StatelessWidget {
   final Widget child;
 
-  const GlassListContainer({super.key, required this.child});
+  const GlassListContainer({
+    super.key,
+    required this.child,
+  });
 
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    final onSurface = theme.colorScheme.onSurface;
+    final appColors = context.appColors;
 
     return Container(
       decoration: BoxDecoration(
-        color: onSurface.withValues(alpha: 0.03),
         borderRadius: BorderRadius.circular(24),
         border: Border.all(
-          color: onSurface.withValues(alpha: 0.05),
+          color: appColors.scaffoldLine,
           width: 0.5,
         ),
       ),
       child: ClipRRect(
         borderRadius: BorderRadius.circular(24),
         child: LiquidGlassOverlay(
-          speedFactor: 1.2,
-          accentColor: onSurface.withValues(alpha: 0.8),
+          intensity: 0.85,
           child: child,
         ),
       ),
