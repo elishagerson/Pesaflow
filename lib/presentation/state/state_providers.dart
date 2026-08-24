03daac19c604a58e988570e899451046c5ce6916
Elisha Gerson <ndunduluelisha@gmail.com>
2026-08-24 12:57:01 +0300

    fix: update state providers

--- lib/presentation/state/state_providers.dart
+++ lib/presentation/state/state_providers.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:drift/drift.dart' hide Column;
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../data/database/app_database.dart';
