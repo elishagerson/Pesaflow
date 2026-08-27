747434a4ccf1c6d968113600eaed5ce0a0680bec
Elisha Gerson <ndunduluelisha@gmail.com>
2026-08-27 13:50:43 +0300

    feat: add tests for mixx parser

---
+++ test/mixx_parser_test.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:pesaflow/domain/sms/provider_matcher.dart';
+import 'package:pesaflow/domain/sms/parsers/mixx_parser.dart';
+
+void main() {
+  test('Mixx parser payment successful to sporty bet', () {
+    final text = "Payment Successful to Sporty Bet PUSH, Amount TSh 25,000. New Balance TSh 370. Charges TSh 500. VAT TSh 76. TxnID: 26607196230775.26/08/26 19:12.";
+    final provider = ProviderMatcher.matchProvider('Mixx BY Yas', body: text);
+    print('Provider: $provider');
+    expect(provider, 'TigoPesa_TZ');
+    
+    final parsed = MixxParser().parse(text, DateTime.now());
+    expect(parsed, isNotNull);
+    print('Parsed: amount=${parsed?.amount}, type=${parsed?.type}, recipient=${parsed?.senderOrRecipient}, fee=${parsed?.feeAmount}, ref=${parsed?.reference}, bal=${parsed?.balanceAfter}');
+  });
+}
---
+++ test_parse.dart
@@ -0,0 +1,18 @@
+import 'package:pesaflow/domain/sms/sms_processor.dart';
+import 'package:pesaflow/domain/sms/provider_matcher.dart';
+import 'package:pesaflow/domain/sms/parsers/mixx_parser.dart';
+
+void main() {
+  final text = "Payment Successful to Sporty Bet PUSH, Amount TSh 25,000. New Balance TSh 370. Charges TSh 500. VAT TSh 76. TxnID: 26607196230775.26/08/26 19:12.";
+  final provider = ProviderMatcher.matchProvider('Mixx BY Yas', body: text);
+  print('Provider: \$provider');
+  
+  if (provider == 'TigoPesa_TZ') {
+    final parsed = MixxParser().parse(text, DateTime.now());
+    if (parsed == null) {
+      print('Parser returned null!');
+    } else {
+      print('Parsed: amount=\${parsed.amount}, type=\${parsed.type}, recipient=\${parsed.senderOrRecipient}, fee=\${parsed.feeAmount}, ref=\${parsed.reference}, bal=\${parsed.balanceAfter}');
+    }
+  }
+}
