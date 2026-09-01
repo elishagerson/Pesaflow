8e3d885936510bdfd0b8aa64a2f99fdc98d5316d
Elisha Gerson <ndunduluelisha@gmail.com>
2026-09-01 13:00:08 +0300

    fix: update notification service

--- lib/services/notification_service.dart
+++ lib/services/notification_service.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:io' show Platform;
 import 'package:pesaflow/core/utils/currency_formatter.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
